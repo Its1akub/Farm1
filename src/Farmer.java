399d1cd04b5309ea472fd410c019dc3151d88c97
Its_1akub <jacobspernoga@gmail.com>
2024-03-08 13:13:31 +0100

    add methods

---
+++ src/Farmer.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Farmer {
+    private final ArrayList<Flower> flowers = new ArrayList<>();
+    private int fieldSize = 100;
+    private Farm farm = new Farm();
+    private int money = 100;
+
+    public void addFlower(Flower flower) {
+        if (flower.getNeededArea() <= fieldSize) {
+            flowers.add(flower);
+            fieldSize -= (int) flower.getNeededArea();
+        }
+    }
+
+    public void harvestFlower(Flower flower) {
+        if (flowers.contains(flower)) {
+            flowers.remove(flower);
+            fieldSize += (int) flower.getNeededArea();
+        }
+    }
+
+    public void waterFlower(Flower flower) {
+        if (flowers.contains(flower)) {
+            flower.setChanceOfGrowth(flower.getChanceOfGrowth() * 0.9);
+            if (flower.getChanceOfGrowth() < 0.5) {
+                harvestFlower(flower);
+            }
+        }
+    }
+
+    public void addAnimal(Animal animal) {
+        farm.addAnimal(animal);
+    }
+
+    public void removeAnimal(Animal animal) {
+        farm.removeAnimal(animal);
+    }
+
+}
