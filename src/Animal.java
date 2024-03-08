b6b64795c7960825bf87d15d1d68bec7542d0670
Its_1akub <jacobspernoga@gmail.com>
2024-03-08 13:13:37 +0100

    add methods

--- src/Animal.java
+++ src/Animal.java
@@ -5,6 +5,13 @@ public abstract class Animal {
     protected Size size;
     protected String specialCare;
 
+    public Animal(String type, String name, Size size, String specialCare) {
+        setType(type);
+        setName(name);
+        setSize(size);
+        setSpecialCare(specialCare);
+    }
+
     @Override
     public String toString() {
         return "Animal{" +
@@ -13,5 +20,41 @@ public abstract class Animal {
                 ", size=" + size +
                 ", specialCare='" + specialCare + '\'' +
                 '}';
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        if (size == Size.SMALL){
+            this.size = Size.SMALL;
+        }else if (size == Size.BIG){
+            this.size = Size.BIG;
+        }
+    }
+
+    public String getSpecialCare() {
+        return specialCare;
+    }
+
+    public void setSpecialCare(String specialCare) {
+        this.specialCare = specialCare;
     }
 }
